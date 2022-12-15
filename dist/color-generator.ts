b33a967be75986b753caca430410353753f38e70
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2022-12-15 15:11:46 -0300

    Removed console.log messages and adjusted types

--- dist/color-generator.ts
+++ dist/color-generator.ts
@@ -3,11 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ColorGenerator {
   
-  constructor() {
-    console.log('created');
-  }
+  constructor() {}
 
-  COLORS: any[] = ['#e57373', '#f06292', '#ba68c8', '#9575cd', '#7986cb', '#64b5f6',
+  COLORS: string[] = ['#e57373', '#f06292', '#ba68c8', '#9575cd', '#7986cb', '#64b5f6',
     '#4fc3f7', '#4dd0e1', '#4db6ac', '#81c784', '#aed581', '#ff8a65', '#d4e157', '#673ab7',
     '#ffb74d', '#a1887f', '#90a4ae'];
 
